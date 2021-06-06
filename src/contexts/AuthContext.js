874fd6cd19cc30793d3350c9e17d98d5b49a1580
ravi shankar <shankarji1729@gmail.com>
2021-06-06 19:41:00 +0530

    Implimented context provider and authantication

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,35 @@
+import reactDom from "react-dom";
+import React, { useContext, useState, useEffect } from 'react'
+import { useHistory } from "react-router";
+import auth from '../firebase'
+
+
+const AuthContext=React.createContext()
+
+export const useAuth=()=>useContext(AuthContext)
+
+export const AuthProvider=({children})=>{
+    const [loading,setLoading]=useState(true)
+    const [user,SetUser]=useState({})
+    const history =useHistory()
+
+    useEffect(()=>{
+        auth.onAuthStateChanged((user)=>{
+                SetUser(user)
+                setLoading(false)
+                history.push('/chats')
+        })
+
+    },[user,history])
+
+    const value={user}
+
+    return(
+        <AuthContext.Provider value={value}>
+                {!loading && children}
+        </AuthContext.Provider>
+    )
+}
 
 
 
